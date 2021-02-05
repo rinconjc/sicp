e945d7840623ec6091b82c6b8139d06bcbebd6f2
Julio Rincon <jcrinconc@gmail.com>
2021-02-06 10:12:42 +1100

    streams

--- sicp.clj
+++ sicp.clj
@@ -8,8 +8,12 @@
 
 (def integers (ints-from 1))
 
-(defn divisible? [n d]
-  (zero? (rem n d)))
+(defn divisible? 
+  ([n d] (zero? (rem n d)))
+  ([n d & [e more]] (or (divisible? n d) (apply divisible? n more))))
+
+(println "div 5 3" (divisible? 5 3))
+(println "div 15 3,2,5" (divisible? 15 3 2))
 
 (defn sieve [s]
   (cons (first s)
@@ -46,3 +50,32 @@
             (triples (rest s) (rest t) (rest u))))))
 
 (println "triples:" (take 20 (triples integers integers integers)))
+
+(def pythagoras 
+  (filter (fn[[x y z]] (= (* z z) (+ (* x x) (* y y))))
+          (triples integers integers integers)))
+
+(println "pythagoras:" (take 6  pythagoras))
+
+(defn merge-weighted [s t w]
+  (let [s0 (first s)
+        t0 (first t)]
+    (if (<= (w s0) (w t0))
+      (cons s0 (lazy-seq (merge-weighted (rest s) t w)))
+      (cons t0 (lazy-seq (merge-weighted s (rest t) w))))))
+
+(defn ordered-pairs [s t w]
+  (cons [(first s) (first t)]
+        (lazy-seq
+          (merge-weighted
+           (map #(vector (first s) %) (rest t)) 
+           (ordered-pairs (rest s) (rest t) w)
+           w))))
+
+(println "ordered pairs by sum:" (time (take 10 (ordered-pairs integers integers #(reduce + %)))))
+
+(def pairs-not-div-by-2-3-5 
+  (filter (fn[[i j]] (some #(some divisible?  ) [i j])))
+
+
+
